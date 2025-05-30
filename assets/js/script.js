1b1f6ca887cca0b16fd114cade7d7128e0e78415
José C. Júnior <lyojunior7@gmail.com>
2025-05-30 18:47:59 -0300

    add logic in modal and cart

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,12 @@ const cartCounter = document.getElementById('cart_count')
 const adressInput = document.getElementById('adress')
 const adressFailed = document.getElementById('adress_error')
 
+let cart = []
+
 
 /* --------------- Abrir Modal ---------------*/
 cartBtn.addEventListener("click", function() {
+    updateCartModal()
     cartModal.style.display = "flex"
 })
 
@@ -28,3 +31,56 @@ fecharModal.addEventListener("click", function(event){
 })
 /* ---------------------------------------------*/
 
+menu.addEventListener("click", function(){
+    let parentButton = event.target.closest(".icon")
+    //console.log(parentButton)
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        addToCart(name, price)
+    }
+})
+
+function addToCart(name, price) {
+    const repeatItem = cart.find(item => item.name === name)
+    if(repeatItem){
+        repeatItem.quantity += 1
+    }else {
+        cart.push({
+            name,
+            price,
+            quantity: 1,
+        })
+
+        updateCartModal()
+    }
+
+
+}
+
+function updateCartModal(){
+    cartItensContainer.innerHTML = ""
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("section")
+
+        cartItemElement.innerHTML = `
+            <section>
+                <section>
+                    <p>${item.name}</p>
+                    <p>${item.quantity}</p>
+                    <p>${item.price} R$</p>
+                </section>
+
+                <section>
+                    <button>
+                        Remover
+                    </button>
+                </section>
+            </section>
+        `
+
+        cartItensContainer.appendChild(cartItemElement)
+    })
+}
