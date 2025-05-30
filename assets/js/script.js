0f81bca9ec07b01cb8053d6cc239de7031f1b1b6
José C. Júnior <lyojunior7@gmail.com>
2025-05-30 12:25:08 -0300

    modify structure const´s this scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-const menu = document.getElementById('menu');
-const cartBtn = document.getElementById('cart_btt');
-const cartModal = document.getElementById('modal');
-const cartItensContainer = document.getElementById('cart_itens');
-const cartTotal = document.getElementById('cart_total');
-const checkoutBtn = document.getElementById('checkout_btt');
-const fecharModal = document.getElementById('close_modal');
-const addToCartButtons = document.querySelectorAll('.icon');
-const cartCounter = document.getElementById('cart_count');
-const adressInput = document.getElementById('adress');
-const adressFailed = document.getElementById('adress_error');
+const menu = document.getElementById('menu')
+const cartBtn = document.getElementById('cart_btt')
+const cartModal = document.getElementById('modal')
+const cartItensContainer = document.getElementById('cart_itens')
+const cartTotal = document.getElementById('cart_total')
+const checkoutBtn = document.getElementById('checkout_btt')
+const fecharModal = document.getElementById('close_modal')
+const addToCartButtons = document.querySelectorAll('.icon')
+const cartCounter = document.getElementById('cart_count')
+const adressInput = document.getElementById('adress')
+const adressFailed = document.getElementById('adress_error')
 
 cartBtn.addEventListener("click", function() {
-    cartModal.style.display = "flex";
-});
+    cartModal.style.display = "flex"
+})
